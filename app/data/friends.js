c1a4225bb4269fdd127229347b97f78f8b51312b
Nmahabir <natalia.mahabir@gmail.com>
2018-09-13 23:18:05 -0400

    survey questions added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,22 @@
 var friends = [{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name: "Joey",
+    photo: "https://www.google.ca/imgres?imgurl=https%3A%2F%2Fimagesvc.timeincapp.com%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fcdn-img.instyle.com%252Fsites%252Fdefault%252Ffiles%252Fstyles%252F640x768%252Fpublic%252F1510247255%252F110917-friends-joey.jpg%253Fitok%253DY31IvER2%26w%3D400%26q%3D85&imgrefurl=https%3A%2F%2Fwww.instyle.com%2Fcelebrity%2Fmatt-leblanc&docid=E6DikCVR6r4_3M&tbnid=H9NwFdk0tJkDaM%3A&vet=10ahUKEwjE2IqHw7ndAhUU24MKHd7qBW0QMwjaAig0MDQ..i&w=400&h=480&bih=663&biw=1280&q=matt%20leblanc&ved=0ahUKEwjE2IqHw7ndAhUU24MKHd7qBW0QMwjaAig0MDQ&iact=mrc&uact=8",
+    scores: [
+        4,
+        4,
+        3,
+        4,
+        2,
+        4,
+        3,
+        5,
+        4,
+        2
+      ]
+ }, 
+ {
+    name: "Phoebe",
+    photo: "https://www.google.ca/imgres?imgurl=https%3A%2F%2Fi.dailymail.co.uk%2Fi%2Fpix%2F2015%2F08%2F19%2F08%2F2B7B42B800000578-3203064-image-m-21_1439969361125.jpg&imgrefurl=https%3A%2F%2Fwww.dailymail.co.uk%2Ftvshowbiz%2Farticle-3203064%2FShe-s-not-like-Phoebe-Lisa-Kudrow-reveals-s-opposite-Friends-character-not-adventurous-type.html&docid=vC1qkbz4Hst1jM&tbnid=sUDqNdlSq6AohM%3A&vet=10ahUKEwjf7o-gw7ndAhVoxoMKHS5yD1QQMwiLAighMCE..i&w=634&h=670&bih=663&biw=1280&q=phoebe&ved=0ahUKEwjf7o-gw7ndAhVoxoMKHS5yD1QQMwiLAighMCE&iact=mrc&uact=8",
     scores: [
         5,
         1,
@@ -11,6 +27,38 @@ var friends = [{
         2,
         5,
         4,
+        1
+      ]
+ },
+ {
+    name: "Rachel",
+    photo: "https://www.google.ca/imgres?imgurl=https%3A%2F%2Fcdn-img.instyle.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F684xflex%2Fpublic%2F1532533939%2Fjennifer-aniston-001.jpg%3Fitok%3D-sE8e0Is&imgrefurl=https%3A%2F%2Fwww.instyle.com%2Fnews%2Fjennifer-aniston-september-cover&docid=yGxdTEdqS8mYgM&tbnid=J5oHfYuPxpZs_M%3A&vet=10ahUKEwi1lI3YwrndAhWTyIMKHcLUAjsQMwiaAigFMAU..i&w=696&h=938&bih=663&biw=1280&q=jennifer%20aniston&ved=0ahUKEwi1lI3YwrndAhWTyIMKHcLUAjsQMwiaAigFMAU&iact=mrc&uact=8",
+    scores: [
+        1,
+        3,
+        4,
+        5,
+        2,
+        1,
+        5,
+        3,
+        2,
+        1
+      ]
+ },
+ {
+    name: "Chandler",
+    photo: "https://www.google.ca/imgres?imgurl=https%3A%2F%2Fewedit.files.wordpress.com%2F2016%2F08%2Fgettyimages-138426899.jpg&imgrefurl=https%3A%2F%2Few.com%2Fgallery%2Ffriends-chandler-best-moments&docid=5JRdxexwen4uXM&tbnid=QEWIJgAm-BwxWM%3A&vet=10ahUKEwjn273Fw7ndAhVj6oMKHWj2CywQMwjxASgCMAI..i&w=612&h=380&bih=663&biw=1280&q=chandler&ved=0ahUKEwjn273Fw7ndAhVj6oMKHWj2CywQMwjxASgCMAI&iact=mrc&uact=8",
+    scores: [
+        1,
+        3,
+        4,
+        5,
+        2,
+        1,
+        5,
+        3,
+        2,
         1
       ]
  }]
